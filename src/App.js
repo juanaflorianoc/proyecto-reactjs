eba7ccf51f0dbae8678622a40fab22a0c5510401
Jauana Floriano Carrasco <florianojuana@gmail.com>
2022-04-17 16:21:11 -0300

    itemlistcontainer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
+
+
+  const estilos = {
+    color:'#D61F69'
+  }
+
+
   return (
     <div className="App">
 
       <NavBar/>
       
-      <h1>Hola mundo</h1>
+      <h1 className='text-4xl' style={estilos}>Hola mundo</h1>
+
+      <ItemListContainer/>
       
       {/*<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ItemListContainer = () => {
+  return (
+
+    <div>
+        <br></br>
+        <br></br>
+         <p>
+             ¡Hola! Nosotras somos Cambalache y vendemos ropa usada en perfecto estado. Próximamente nuestra lista de productos la verás aquí abajo.
+         </p>
+    </div>
+
+  )
+}
+
+export default ItemListContainer
